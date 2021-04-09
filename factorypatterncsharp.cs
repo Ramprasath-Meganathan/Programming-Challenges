6fe0b8682ad990272b257e24ef6a091a19e9fd29
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2021-04-09 00:48:49 -0300

    csharp factor pattern code final

--- factorypatterncsharp.cs
+++ factorypatterncsharp.cs
@@ -1,77 +1,77 @@
-using System;
+ using System;
 
-namespace factoryPattern
-{
-public interface IButton
-{
-string click();
-}
-
-class WebButton: IButton
-{
-    public string click()
-    {   
-        return "{returns a new web button click}";
-    }
-}
-
-class MobileButton: IButton
-{
-    public string click()
+    namespace factoryPattern
     {
-        return "{returns a new mobile button click}";
-    }
-}
-
-abstract class Dialog 
-{
-    public abstract IButton FactoryMethod();
-    public string someoepration()
-    {
-        var button = FactoryMethod();
-        return "creators code working with "+button.click();
-    }
-}
-
-public class WebDialog : Dialog
-{
-    public override FactoryMethod()
-    {
-        return new WebButton();
-    }
-
-}
-public class MobileDialog : Dialog
-{
-    public override FactoryMethod()
-    {
-        return new MobileButton();
-    }
-
-}
-
-class ClientClass
-{
-        public  void main()
-
-    {
-        ClientCall(new WebDialog());
-        ClientCall(new MobileDialog());
-}
-
- public void ClientCall(Dialog dialog)
+        public interface IButton
         {
-            Console.WriteLine("Client: I'm not aware of the creator's class," +
-                "but it still works.\n" + dialog.SomeOperation());
-            // ...
+            string click();
         }
 
-
-class MainClass
-{
-    static void Main(string[] args)
+        class WebButton : IButton
         {
-            new ClientClass().Main();
+            public string click()
+            {
+                return "{returns a new web button click}";
+            }
         }
-}
-}
+
+        class MobileButton : IButton
+        {
+            public string click()
+            {
+                return "{returns a new mobile button click}";
+            }
+        }
+
+        public abstract class Dialog
+        {
+            public abstract IButton FactoryMethod();
+            public string SomeOperation()
+            {
+                var button = FactoryMethod();
+                return "creators code working with " + button.click();
+            }
+        }
+
+        public class WebDialog : Dialog
+        {
+            public override IButton FactoryMethod()
+            {
+                return new WebButton();
+            }
+
+        }
+        public class MobileDialog : Dialog
+        {
+            public override IButton FactoryMethod()
+            {
+                return new MobileButton();
+            }
+
+        }
+
+        class ClientClass
+        {
+            public void Main()
+
+            {
+                ClientCall(new WebDialog());
+                ClientCall(new MobileDialog());
+            }
+
+            public void ClientCall(Dialog dialog)
+            {
+                Console.WriteLine("Client: I'm not aware of the creator's class," +
+                    "but it still works.\n" + dialog.SomeOperation());
+                // ...
+            }
+
+
+            class MainClass
+            {
+                static void Main(string[] args)
+                {
+                    new ClientClass().Main();
+                }
+            }
+        }
